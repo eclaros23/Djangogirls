3c214c4aa83cb1419ce076e1f746ba6161c031c4
Emily <emicla23@bergen.org>
2023-03-08 18:45:18 +0000

    Made changes to the test.py file

--- blog/tests.py
+++ blog/tests.py
@@ -13,7 +13,7 @@ class PostListTest (TestCase):
     
   def test_noteposts(self): 
       """When there are no posts on the page then the page loads successfully"""
-      response = self.client.get("/") # url of a page is /
+      response = self.client.get("/") # url 
       self.assertEqual(response.status_code, 200) # django assertEqual method (status code of 200 = success)
       self.assertEqual(response.context["posts"].count(), 0) #loading list of posts without posts
 
@@ -100,7 +100,7 @@ class PostEdit(TestCase):
     self.assertEqual(response.status_code, 200)
 
   def test_get_loggedout(self):
-    """If user is logged they won't be able to edit posts"""
+    """If user is logged out they won't be able to edit posts"""
     self.client.logout()
     author = User.objects.create(username = "user1") 
     published_post = Post.objects.create(title = "Post01", text = "Hey", author = author, published_date = now())
@@ -111,7 +111,7 @@ class PostEdit(TestCase):
     self.assertEqual(response.status_code, 404) 
 
   def test_post_published(self):
-    """If a post is published then the user should be able to edit it"""
+    """If a post is published then the user should be able to edit posts"""
     author = User.objects.create(username = "user1") 
     published_post = Post.objects.create(title = "Post01", text = "Hey", author = author, published_date = now())
     response = self.client.post(f"/post/{published_post.pk}/edit/", {"title": "new title", "text": "new text"}) 
@@ -121,7 +121,7 @@ class PostEdit(TestCase):
     self.assertEqual(published_post.text, "new text")
 
   def test_post_unpublished(self):
-    """If a post is unpublished then the user should not be able to edit it"""
+    """If a post is unpublished then the user should be able to edit it"""
     author = User.objects.create(username = "user1")
     unpublished_post = Post.objects.create(title = "Post02", text = "Great", author = author)
     response = self.client.post(f"/post/{unpublished_post.pk}/edit/", {"title": "new title", "text": "new text"})
@@ -131,7 +131,7 @@ class PostEdit(TestCase):
     self.assertEqual(unpublished_post.text, "new text")
 
   def test_post_loggedout(self):
-    """If user is logged they won't be able to edit posts"""
+    """If user is logged out they won't be able to edit posts"""
     self.client.logout()
     author = User.objects.create(username = "user1") 
     published_post = Post.objects.create(title = "Post01", text = "Hey", author = author, published_date = now())
@@ -141,6 +141,13 @@ class PostEdit(TestCase):
     response = self.client.post(f"/post/{unpublished_post.pk}/edit/") 
     self.assertEqual(response.status_code, 404) 
 
+  def test_post_nonexistent(self):
+    """If post does not exist the user won't be able to edit posts"""
+    response = self.client.post("/post/1000000/edit/")
+    self.assertEqual(response.status_code, 404)
+    response = self.client.post("/post/!!!!!/edit/")
+    self.assertEqual(response.status_code, 404)
+  
 class PostNewTest (TestCase):
   def setUp(self): # setting up username and password for every test
       user = User.objects.create(username = "test_user")
@@ -172,12 +179,15 @@ class PostNewTest (TestCase):
     response = self.client.post("/post/new/", {"title": "new title", "text": "new text"})
     self.assertEqual(response.status_code, 404)
 
-  
-
-  
-
-    
-    
-
-
-    
+  def test_post_title_exists(self):
+    """If user creates post with the same title as another post that exists they should get an error"""
+    response = self.client.post('/post/new/')
+    author = User.objects.create(username = "user1") 
+    published_post = Post.objects.create(title = "Post01", text = "Good", author = author)
+    response = self.client.post('/post/new/', {"title": "Post01", "text": "Good"})
+    self.assertEqual(response.context["form"].errors, {"__all__": ["Post Title Exists!"]})
+    self.assertEqual(response.status_code, 200)
+    unpublished_post = Post.objects.create(title = "Post02", text = "Great", author = author)
+    response = self.client.post('/post/new/', {"title": "Post02", "text": "Nice"})
+    self.assertEqual(response.context["form"].errors, {"__all__": ["Post Title Exists!"]})
+    self.assertEqual(response.status_code, 200)
