7d2fb7112914c5042bfe8597071964ba41f1408b
Emily <emicla23@bergen.org>
2023-03-08 18:46:07 +0000

    Made changes to forms.py file

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,15 @@ from django import forms
 
 from .models import Post
 
-class PostForm(forms.ModelForm):
+from django.core.exceptions import ValidationError
 
+class PostForm(forms.ModelForm):
+    def clean(self):
+        clean_data = super().clean()
+        if Post.objects.filter(title = clean_data.get("title", "")).exists():
+            raise ValidationError ("Post Title Exists!")
+        return clean_data
+      
     class Meta:
         model = Post
         fields = ('title', 'text')
